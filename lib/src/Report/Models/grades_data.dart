1875cda354f209e4a5a7eb29b7a66f62a3dcdcdd
Nutthanitcha.n <85213505+jintopia07@users.noreply.github.com>
2022-07-01 15:51:16 +0700

    Delete lib/src/Report directory

--- lib/src/Report/Models/grades_data.dart
+++
@@ -1,7 +0,0 @@
-// @dart=2.9
-class GradesData {
-  final String gradeSymbol;
-  final int numberOfStudents;
-
-  GradesData(this.gradeSymbol, this.numberOfStudents);
-}
--- lib/src/Report/Models/sales_data.dart
+++
@@ -1,7 +0,0 @@
-// @dart=2.9
-class SalesData {
-  final int year;
-  final int sales;
-
-  SalesData(this.year, this.sales);
-}
