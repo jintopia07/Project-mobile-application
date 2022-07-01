a01ea1205bbcced833c62ff07a589b0533ca8fc1
Nutthanitcha.n <85213505+jintopia07@users.noreply.github.com>
2022-07-01 11:57:58 +0700

    Update WebSite_Config.dart

--- lib/Config/WebSite_Config.dart
+++ lib/Config/WebSite_Config.dart
@@ -1,8 +1,8 @@
 class WebSite_Config {
-  static String mainWebSiteURL = 'http://archives.nat.go.th';
-  static String mainWebSiteURLWithS = "https://archives.nat.go.th";
+  static String mainWebSiteURL = '';
+  static String mainWebSiteURLWithS = "";
   static String mainWebSiteURLWithDigitalFileFolder =
-      "http://archives.nat.go.th/digitalfiles/";
+      "";
   static String mainWebSiteURLWithSAndDigitalFileFolder =
-      "https://archives.nat.go.th/digitalfiles/";
+      "";
 }
