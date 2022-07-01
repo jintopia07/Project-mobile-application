592d7ceededba8adca5c8eaf6ddff6bb6cc02c83
Nutthanitcha.n <85213505+jintopia07@users.noreply.github.com>
2022-07-01 11:57:00 +0700

    Update WebAPI_Config.dart

--- lib/Config/WebAPI_Config.dart
+++ lib/Config/WebAPI_Config.dart
@@ -1,7 +1,4 @@
 class WebAPI_Config {
-  // static final String mainWebAPIURL = 'http://apiservice.nat.go.th';
-  //static const String mainWebAPIURL = 'http://192.168.217.122';
-  //  static final String mainWebAPIURL = 'http://dev.archivesapi.nat.go.th';
   static final String mainWebAPIURL = 'http://192.168.217.128:1111';
    
 }
